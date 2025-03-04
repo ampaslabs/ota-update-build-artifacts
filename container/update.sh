3edcd1a6796fb6cf917f49c2d6bfdc3180833a0f
Ganesh Velrajan <gannygans@gmail.com>
2025-03-04 12:04:04 +0530

    container ota update workflow scripts updated

--- container/update.sh
+++ container/update.sh
@@ -1,183 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 
-#================================================
-# Docker Container Update Workflow Script. 
-# Artifact Registry: AWS Registry or Docker Hub.
-#================================================
-
-# Set AWS credentials
-AWS_ACCESS_KEY_ID="ABCDEFGHIJKLMNOPQ"                                                 
-AWS_SECRET_ACCESS_KEY="AbcDeFgh1234pqRstUvW6789xyZ" 
-AWS_REGISTRY_ADDRESS="123456789012.dkr.ecr.us-east-2.amazonaws.com"                        
-
-# AWS (Elastic Container Registry) ECR login/logout commands 
-AWS_ECR_LOGIN_COMMAND="docker login --username ${AWS_ACCESS_KEY_ID} --password-stdin  ${AWS_REGISTRY_ADDRESS}"
-AWS_ECR_LOGOUT_COMMAND="docker logout ${AWS_REGISTRY_ADDRESS}"
-
-# Set Docker Hub credentials
-DOCKER_HUB_USERNAME="dockerhub-username"
-DOCKER_HUB_ACCESS_TOKEN="75fa718c-9ec3-11ec-b909-0242ac120002"
-
-# Docker Hub container registry login/logout commands
-DOCKER_HUB_LOGIN_COMMAND="docker login --username ${DOCKER_HUB_USERNAME} --password-stdin"
-DOCKER_HUB_LOGOUT_COMMAND="docker logout"
-
-# Log debugs
-debug_log() {
-    echo "$(date "%Y-%m-%d %H:%M:%S.%N"): $1"
-}
-
-# Clean Up
-cleanup() {
-   debug_log "Cleaning up..."
-   # Logout of the docker image registry: DockerHub or AWS ECR
-   $DOCKER_HUB_LOGOUT_COMMAND                                                                  
-   # $AWS_ECR_LOGOUT_COMMAND                                                                  
-   rm /tmp/update.sh  
-   debug_log "Clean up done!"                                                     
-}
-
-# Log error and exit
-debug_error_exit() {
-    echo "$(date "%Y-%m-%d %H:%M:%S.%N"): Error: $1"
-    cleanup
-    exit 1
-}
-
-# Restore previously running container
-restore_from_backup() {
-    DOCKER_RUN_COMMAND=$1
-    CONTAINER_NAME=$2
-
-    debug_log "Stopping and removing failed container: ${CONTAINER_NAME}" 
-    docker rm --force ${CONTAINER_NAME}
-
-    debug_log "Restoring from backup: $DOCKER_RUN_COMMAND"
-    eval $1
-         if [ $? -eq 0 ]; then                                                                   
-            debug_log "Docker run command executed: ${DOCKER_RUN_COMMAND}"                                      
-         else                                                                                    
-            debug_error_exit "Docker run command failed!: ${DOCKER_RUN_COMMAND}"
-         fi
-    # Sleep 30 seconds before checking the status
-    sleep 30
-    CONTAINER_RUNNING_STATUS="$(docker inspect --format "{{.State.Running}}" --type container "${CONTAINER_NAME}")"
-        if [ $? -eq 0 ]; then                                                                         
-            debug_log "Container running status: ${CONTAINER_RUNNING_STATUS}"                                               
-        else                                                                                          
-            debug_error_exit "Failed retrieving container running status!: ${CONTAINER_NAME}"                                                    
-        fi 
-
-    if [[ "$CONTAINER_RUNNING_STATUS" == "true" ]]; then
-        debug_log "Restoration successful: ${CONTAINER_NAME}"
-    else 
-        debug_error_exit "Restoration failed: ${CONTAINER_NAME}"    
-    fi  
-}
-
-#============================================
-# Workflow Begins.
-#============================================  
-
-# Get the container name and the latest image name from the command line args                             
-container=$1                                                         
-echo "Container name: ${container}" 
-LATEST_IMAGE_NAME=$2
-echo "Image name: ${LATEST_IMAGE_NAME}"
-
-# Login to the docker image registry: DockerHub or AWS ECR
-echo "${DOCKER_HUB_ACCESS_TOKEN}" | $($DOCKER_HUB_LOGIN_COMMAND)
-# echo "${AWS_SECRET_ACCESS_KEY}" | $($AWS_ECR_LOGIN_COMMAND)  
+DOCKER_USER="dockerhub-username"
+DOCKER_REPO="myapp"
+NEW_VERSION="1.0.0"
+OLD_VERSION="0.0.1"
+CONTAINER_NAME="myapp"
 
 
-# To update a list of containers, use the below for-loop example:
-#
-# CONTAINERS=$1
-# LATEST_IMAGE_NAMES=$2
-#
-# for i in ${!CONTAINERS[@]}; do 
-#   container=${CONTAINERS[i]}
-#   LATEST_IMAGE_NAME=${LATEST_IMAGE_NAMES[i]}
-#   ...
-#   ...
-# done
+# Function to check if a container is running and healthy
+check_container_status() {
+    local container_id="$1"
+    local status=$(docker inspect --format='{{.State.Health.Status}}' "$container_id" 2>/dev/null)
+    local state=$(docker inspect --format='{{.State.Running}}' "$container_id" 2>/dev/null)
 
-# Update Begin                                                                                                
-echo "\n+++ Begin updating container: ${container} +++\n"
-RUNNING_IMAGE_HASH="$(docker inspect --format "{{.Image}}" --type container "${container}")"       
-    if [ $? -eq 0 ]; then                                                                         
-    debug_log "Running container's image hash: ${RUNNING_IMAGE_HASH}"                                                   
-    else                                                                                          
-    debug_error_exit "Failed retrieving running container's image hash: ${container}"                                                      
-    fi                                                                                            
-
-# Pull the latest version of the container and get the hash                                    
-docker pull "${LATEST_IMAGE_NAME}"                                                              
-    if [ $? -eq 0 ]; then                                                                         
-    debug_log "docker pull of latest image successful: ${LATEST_IMAGE_NAME}"                                                   
-    else                                                                                          
-    debug_error_exit "docker pull of latest image failed: ${LATEST_IMAGE_NAME}"                                                        
-    fi              
-
-# Get the latest image hash                                                                              
-LATEST_IMAGE_HASH="$(docker inspect --format "{{.Id}}" --type image "${LATEST_IMAGE_NAME}")"         
-    if [ $? -eq 0 ]; then                                                                         
-    debug_log "Latest image hash: ${LATEST_IMAGE_HASH}"                                                     
-    else                                                                                          
-    debug_error_exit "Failed retrieving the latest image's hash: ${LATEST_IMAGE_NAME}"                                                       
-    fi                                                                                            
-
-# Create a new container using the latest image, if the image is different                                             
-if [[ "${RUNNING_IMAGE_HASH}" != "${LATEST_IMAGE_HASH}" ]]; then                                        
-    debug_log "Updating container ${container} with latest image ${LATEST_IMAGE_NAME}"                                        
-
-    # Delete the running container first                                                
-    docker rm --force "${container}"                                                            
-        if [ $? -eq 0 ]; then                                                                   
-        debug_log "docker rm: ${container}"                                                         
-        else                                                                                    
-        debug_error_exit "docker rm failed!: ${container}"                                                        
-        fi    
-
-    # Create a new container using the latest image
-    DOCKER_RUN_COMMAND="docker run -d --name ${container}"  
-    eval "${DOCKER_RUN_COMMAND} ${LATEST_IMAGE_NAME}"                                                               
-        if [ $? -eq 0 ]; then                                                                   
-        debug_log "docker run command executed: ${DOCKER_RUN_COMMAND} ${LATEST_IMAGE_NAME}"                                      
-        else                                                                                    
-        debug_log "docker run command failed!: ${DOCKER_RUN_COMMAND} ${LATEST_IMAGE_NAME}" 
-        restore_from_backup "$(${DOCKER_RUN_COMMAND} ${RUNNING_IMAGE_HASH})" "${container}"
-        fi 
-
-    # Sleep 30 seconds before checking the container status
-    sleep 30
-    CONTAINER_RUNNING_STATUS="$(docker inspect --format "{{.State.Running}}" --type container "${container}")"
-        if [ $? -eq 0 ]; then                                                                         
-          debug_log "Container running status: ${CONTAINER_RUNNING_STATUS}"                                               
-        else                                                                                          
-          debug_log "Failed retrieving container running status!: ${container}" 
-          restore_from_backup "${DOCKER_RUN_COMMAND} ${RUNNING_IMAGE_HASH}" "${container}"
+    if [[ "$state" == "true" ]]; then
+        if [[ -z "$status" ]]; then
+            return 0 # Running, no healthcheck
+        elif [[ "$status" == "healthy" ]]; then
+            return 0 # Running and healthy
+        else
+            return 1 # Running, but unhealthy
         fi
-
-    if [[ "$CONTAINER_RUNNING_STATUS" == "true"  ]]; then
-        debug_log "Container update successful: ${container}"
     else
-        debug_log "Container update failed: ${container}"
-        restore_from_backup "${DOCKER_RUN_COMMAND} ${RUNNING_IMAGE_HASH}" "${container}"
+        return 1 # Not running
     fi
+}
 
-    # Remove the old image                                                                                     
-    docker rmi "${RUNNING_IMAGE_HASH}"                                                               
-        if [ $? -eq 0 ]; then                                                                   
-        debug_log "docker rmi: ${RUNNING_IMAGE_HASH}"                                                    
-        else                                                                                    
-        debug_error_exit "docker rmi failed!: ${RUNNING_IMAGE_HASH}"                                                       
-        fi                                                                                      
-else                                                                                          
-  echo "Running image same as the latest image. Not updating ${container} with latest image ${LATEST_IMAGE_NAME}"                
+# Function to rollback to the previous version
+rollback() {
+    echo "Rolling back to version $OLD_VERSION..."
+
+    # Stop and remove the new container
+    docker stop "${CONTAINER_NAME}-${NEW_VERSION}" 2>/dev/null
+    docker rm "${CONTAINER_NAME}-${NEW_VERSION}" 2>/dev/null
+
+    # Delete the new image
+    docker image rm "$DOCKER_USER/$DOCKER_REPO:$NEW_VERSION" 2>/dev/null
+
+
+    # Run the old container
+    docker run -d "$DOCKER_USER/$DOCKER_REPO:$OLD_VERSION"
+}
+
+# Main update process
+echo "Updating to version $NEW_VERSION..."
+
+# Pull the new image
+docker pull "$DOCKER_USER/$DOCKER_REPO:$NEW_VERSION"
+
+# Stop the old container
+docker stop "$${CONTAINER_NAME}-${OLD_VERSION}" 2>/dev/null
+
+# Run the new container
+docker run -d --name ${CONTAINER_NAME}-${NEW_VERSION} "$DOCKER_USER/$DOCKER_REPO:$NEW_VERSION"
+
+# Check container status
+sleep 30 # Give the container time to start and healthcheck to run. Adjust as needed.
+container_id=$(docker ps -q --filter name=${CONTAINER_NAME}-${NEW_VERSION})
+
+if [[ -n "$container_id" ]] && check_container_status "$container_id"; then
+    echo "Update successful!"
+    # Clean up the old container and image
+    docker rm ${CONTAINER_NAME}-${OLD_VERSION} 2>/dev/null
+    docker image rm "$DOCKER_USER/$DOCKER_REPO:$OLD_VERSION" 2>/dev/null
+else
+    echo "Update failed! Rolling back..."
+    rollback
+    echo "Rollback complete."
 fi
 
-echo "\n+++ End updating container: ${container} +++\n"
-
-# Finally clean up
-cleanup
